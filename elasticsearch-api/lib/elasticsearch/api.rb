3459337abc54cdd40b0dc50b632be4698f421b46
Fernando Briano <fernando@picandocodigo.net>
2019-11-26 13:59:12 +0000

    [API] Add ParamsRegistry

--- elasticsearch-api/lib/elasticsearch/api.rb
+++ elasticsearch-api/lib/elasticsearch/api.rb
@@ -9,8 +9,14 @@ require "elasticsearch/api/version"
 require "elasticsearch/api/namespace/common"
 require "elasticsearch/api/utils"
 require "elasticsearch/api/actions/params_registry"
+require "elasticsearch/api/actions/cat/params_registry"
+require "elasticsearch/api/actions/cluster/params_registry"
+require "elasticsearch/api/actions/indices/params_registry"
+require "elasticsearch/api/actions/ingest/params_registry"
+require "elasticsearch/api/actions/nodes/params_registry"
+require "elasticsearch/api/actions/snapshot/params_registry"
+require "elasticsearch/api/actions/tasks/params_registry"
 
-Dir[ File.expand_path('../api/actions/**/params_registry.rb', __FILE__) ].each   { |f| require f }
 Dir[ File.expand_path('../api/actions/**/*.rb', __FILE__) ].each   { |f| require f }
 Dir[ File.expand_path('../api/namespace/**/*.rb', __FILE__) ].each { |f| require f }
 
